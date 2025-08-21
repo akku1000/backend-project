f6f7606431a64bcd9f31d7d7580ad3d2bff69c2b
akki2332 <akashsoam123456789@gmail.com>
2025-08-21 18:07:57 +0530

    database conect

---
+++ src/db/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+// const mongoose=require('mongoose')
+
+const connectDB=async()=>{
+    try {
+        // const conn= await mongoose.connect(process.env.MONGO_URL);
+        const conn=await mongoose.connect(`${process.env.MONGO_URL}\${DB_NAME}`);
+        console.log(`\n MongoDb connected successfully::HOST:${conn.connection.host}`)
+    } catch (error) {
+        console.log(`error is ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import connectDB from './db/db.js';
+import dotenv from 'dotenv';
+// dotenv.config({ quiet: true });
+dotenv.config();
+connectDB();
